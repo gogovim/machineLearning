acc203ce0e0a57d029466b34fa16c87ff1712400
whao <haohaoac@163.com>
2018-06-24 16:14:45 +0800

    增加剪枝

--- CART_Regression.py
+++ CART_Regression.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pandas as pd
 
-from basic import *
+from basic import getFeatureType
 
-
-class CART_Classification:
+class CART_Regression:
     def __init__(self):
         self.T={}
         self.featureType=[]
+        self.eps=1e-7
     def getSpiltPoint(self,f,t):
         f=np.unique(f)
         return np.linspace(f.min(),f.max(),np.sqrt(len(f)),False) if t else f
@@ -16,19 +16,19 @@ class CART_Classification:
     def getSplitInterval(self,X,by):
         #print(X,by)
         return (X[by],X[by^True])
-    def getGini(self,D):
-        d0=len(D[0])
-        d1=len(D[1])
-        d=d0+d1
-        return giniA(D,[d0/d,d1/d])
+    def getLeastSquares(self,D):
+        c0=np.mean(D[0])
+        c1=np.mean(D[1])
+        return np.sum(np.square(D[0]-c0))+np.sum(np.square(D[1]-c1))
     def gain(self,f,Y,t):
         #print('gain:',f,Y)
         spiltPoint=self.getSpiltPoint(f,t)
         splitArr=list(map(lambda cp:self.getSplitArr(f,cp,t),spiltPoint))
         splitInterval=list(map(lambda arr:self.getSplitInterval(Y,arr),splitArr))
-        splitGini=list(map(lambda D:self.getGini(D),splitInterval))
-        sp=np.argmin(splitGini)
-        return spiltPoint[sp],splitGini[sp]
+        splitLeastSquare=list(map(lambda D:self.getLeastSquares(D),splitInterval))
+        sp=np.argmin(splitLeastSquare)
+        print(spiltPoint[sp],splitLeastSquare[sp])
+        return spiltPoint[sp],splitLeastSquare[sp]
  
     def build(self,X,Y):
         #print('build ',X,Y)
@@ -36,10 +36,9 @@ class CART_Classification:
         numY=np.unique(Y)
         if numY.size==0:
             return None
-        if numY.size==1:
-            T['value']=Y[0]
+        T['value']=np.mean(Y)
+        if np.sum(np.square(Y-np.mean(Y)))<self.eps:
             return T
-        T['value']=pd.value_counts(Y).index[0]
         splitFeature=np.array(list(map(lambda f,t:self.gain(f,Y,t),np.transpose(X),self.featureType)))
         featureIndex=np.argmin(splitFeature[:,1])
         #print('splitFeature:',splitFeature)
@@ -52,31 +51,16 @@ class CART_Classification:
         [(X[spiltArr],Y[spiltArr]),(X[spiltArr^True],Y[spiltArr^True])])
         return T
 
-
-    def findx(self,x,T):
-        if T['key'] is None:
-            return T['value']
-        featureIndex=T['key']
-        if self.featureType[featureIndex]:
-            next=T['left'] if x[featureIndex]<=T['keyValue'] else T['right']
-        else:
-            next=T['left'] if x[featureIndex]==T['keyValue'] else T['right']
-        return T['value'] if next is None else self.findx(x,next)
-    def train(self,X,Y,valX=None,valY=None):
-        self.featureType=getFeatureType(X)
-        self.T=self.build(X,Y)
-        if valX is not None and valY is not None:
-            self.pruningTree(X,Y,valX,valY)
     def getG(self,T,X,Y):
         G={'g':None,'left':None,'right':None}
         if T['key']==None:
             G['g']=np.Inf
-            return G['g'],infoEntropy(Y)*len(Y),len(Y),G
+            return G['g'],np.sum(np.square(Y-T['value'])),len(Y),G
         featureIndex=T['key']
         spiltArr=self.getSplitArr(X[:,featureIndex],T['keyValue'],self.featureType[featureIndex])
         lG,lC,lNum,G['left']=self.getG(T['left'],X[spiltArr],Y[spiltArr])
         rG,rC,rNum,G['right']=self.getG(T['right'],X[spiltArr^True],Y[spiltArr^True])
-        G['g']=(infoEntropy(Y)*len(Y)-lC-rC)/(lNum+rNum)
+        G['g']=(np.sum(np.square(Y-T['value']))-lC-rC)/(lNum+rNum)
         return min(lG,rG,G['g']),lC+rC,lNum+rNum,G
     def cutByalpha(self,T,G,alpha):
         if T['key']==None:
@@ -97,7 +81,7 @@ class CART_Classification:
     def calTError(self,X,Y,T):
         self.T=T
         preY=self.predict(X)
-        return np.sum(preY==Y)/len(Y)
+        return np.sum(np.square(preY-Y))
     def pruningTree(self,X,Y,valX,valY):
         teT=self.copyT(self.T)
         TList=[]
@@ -109,13 +93,26 @@ class CART_Classification:
             print('g:',minG,'G:',G)
         TlistError=list(map(lambda T:self.calTError(valX,valY,T),TList))
         self.T=TList[np.argmin(TlistError)]
-
+    def findx(self,x,T):
+        if T['key'] is None:
+            return T['value']
+        featureIndex=T['key']
+        if self.featureType[featureIndex]:
+            next=T['left'] if x[featureIndex]<=T['keyValue'] else T['right']
+        else:
+            next=T['left'] if x[featureIndex]==T['keyValue'] else T['right']
+        return T['value'] if next is None else self.findx(x,next)
+    def train(self,X,Y,valX=None,valY=None):
+        self.featureType=getFeatureType(X)
+        self.T=self.build(X,Y)
+        if valX is not None and valY is not None:
+            self.pruningTree(X,Y,valX,valY)
     def predict(self,X):
         Y=np.array(list(map(lambda x:self.findx(x,self.T),X)))
         print(list(zip(X,Y)))
         return Y
 if __name__=="__main__":
-    dt=CART_Classification()
+    dt=CART_Regression()
 
     X=np.array([[0,0],[1,1],[0,1],[1,0],[1,1]])
     Y=np.array([0,0,1,1,0])
